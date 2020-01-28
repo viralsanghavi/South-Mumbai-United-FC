2cf334e0d99e790fb0b0f574464d802c4c3da5b7
unknown <vsanghavi@gmail.com>
2020-01-28 22:55:47 +0530

    Navbar Changed

--- SMU/src/app/components/nav-menu/nav-menu.component.ts
+++ SMU/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
+
+
 export class NavMenuComponent implements OnInit {
 
   constructor() { }
+  @HostListener('window:scroll', ['$event'])
 
+  onWindowScroll(e) {
+      let element = document.querySelector('.navbar');
+      if (window.pageYOffset > element.clientHeight) {
+        element.classList.add('navbar-inverse');
+      } else{
+        element.classList.remove('navbar-inverse');
+      }
+    }
+  
   ngOnInit() {
   }
 
@@ -33,4 +46,11 @@ export class NavMenuComponent implements OnInit {
     toggle.classList.remove("display-none");
   }
 
+  shrinkNavbar = false;
+  onScroll(event: any): void {
+    // Shrink the header top and bottom padding when scrolling beyond 300px
+    this.shrinkNavbar =
+      (window.pageYOffset || document.documentElement.scrollTop) > 300;
+  }
 }
+
